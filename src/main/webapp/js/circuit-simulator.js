3c5e1bf23287b6031cc7928c3eca895aef7caa98
Robert Szmurlo <robert.szmurlo@ee.pw.edu.pl>
2015-05-18 08:22:26 +0200

    Dziedziczenie i rysowanie rezystora...

--- src/main/webapp/js/circuit-simulator.js
+++ src/main/webapp/js/circuit-simulator.js
@@ -26,11 +26,7 @@ function init(id) {
 }
 
 function addResistor(x,y) {
-    c = {
-        'x': x,
-        'y': y
-    };
-
+    var c = new Resistor(x,y, "R1");
     components.push(c);
     repaint();
 }
@@ -41,15 +37,62 @@ function repaint() {
 
     for (var i=0; i< components.length; i++) {
         c = components[i];
-        context.beginPath();
-        context.moveTo(c.x, c.y);
-        context.lineTo(c.x+5, c.y);
-        context.lineTo(c.x+5, c.y+5);
-        context.lineTo(c.x, c.y+5);
-        context.lineTo(c.x, c.y);
-        context.closePath();
+        c.paint(context);
     }
 
-    context.stroke();
-
 }
+
+var Terminal = function(dx,dy) {
+    this.dx = dx;
+    this.dy = dy;
+};
+
+var ElectricalComponent = function(x,y,name) {
+    this.x = x;
+    this.y = y;
+    this.name = name;
+    this.terminals = [];
+};
+
+ElectricalComponent.prototype.paint = function (context) {
+    var c = this;
+    for (var i = 0; i < this.terminals.length; i++) {
+        var t = this.terminals[i];
+        context.beginPath();
+        context.moveTo(c.x + t.dx, c.y + t.dy-3);
+        context.lineTo(c.x+6 + t.dx, c.y + t.dy-3);
+        context.lineTo(c.x+6 + t.dx, c.y+3 + t.dy);
+        context.lineTo(c.x + t.dx, c.y+3 + t.dy);
+        context.lineTo(c.x + t.dx, c.y + t.dy-3);
+        context.closePath();
+        context.stroke();
+    }
+};
+
+var Resistor = function(x,y,name) {
+    ElectricalComponent.call(this, x,y,name);
+
+    this.terminals.push( new Terminal(-20,0));
+    this.terminals.push( new Terminal(15,0));
+}
+Resistor.prototype = Object.create(ElectricalComponent.prototype);
+Resistor.prototype.constructor = Resistor;
+Resistor.prototype.paint = function(ctx) {
+    ElectricalComponent.prototype.paint.call(this, ctx);
+
+    //ctx.beginPath();
+    ctx.moveTo(this.x-15, this.y);
+    ctx.lineTo(this.x-12, this.y+5);
+    ctx.lineTo(this.x-9, this.y-5);
+    ctx.lineTo(this.x-6, this.y+5);
+    ctx.lineTo(this.x-3, this.y-5);
+    ctx.lineTo(this.x-0, this.y+5);
+    ctx.lineTo(this.x+3, this.y-5);
+    ctx.lineTo(this.x+6, this.y+5);
+    ctx.lineTo(this.x+9, this.y-5);
+    ctx.lineTo(this.x+12, this.y+5);
+    ctx.lineTo(this.x+15, this.y);
+    //ctx.closePath();
+    ctx.stroke();
+
+};
